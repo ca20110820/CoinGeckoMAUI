33b7808b7254044fc77e064276de0bae4a751efc
ca20110820 <ca20110820@gmail.com>
2024-03-24 18:02:30 +0800

    fix(JsonService): fixed imports

--- CoinGeckoApp/Services/JsonService.cs
+++ CoinGeckoApp/Services/JsonService.cs
@@ -1,7 +1,4 @@
-﻿using GoogleGson;
-using Kotlin.Contracts;
-using Kotlin.Reflect;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
