aee6af89ef70252b5e807310a3bd786d3fea92db
ca20110820 <ca20110820@gmail.com>
2024-03-24 20:08:09 +0800

    feat: initial implementation of APIExchangesListResponse class

---
+++ CoinGeckoApp/Responses/APIExchangesListResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoinGeckoApp.Responses.Exchanges
+{
+    /// <summary>
+    /// Representation of the API Response for Fetching a List of Exchanges.
+    /// <para>This is coming from https://api.coingecko.com/api/v3/exchanges/list.</para>
+    /// </summary>
+    public class APIExchangesListResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
