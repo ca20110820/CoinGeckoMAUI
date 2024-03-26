0155db7ce9cc70706c9a1a554f033bf5d7dd2d51
ca20110820 <ca20110820@gmail.com>
2024-03-26 18:30:33 +0800

    feat(APIService): added FetchAndJsonDeserializeAsync static method for optimized deserialization of large json strings

--- CoinGeckoApp/Services/APIService.cs
+++ CoinGeckoApp/Services/APIService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,6 +39,34 @@ namespace CoinGeckoApp.Services
                 HttpResponseMessage response = await client.GetAsync(url);
 
                 return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null;
+            }
+        }
+
+        /// <summary>
+        /// Fetch and Deserialize the Json string with the given type.
+        /// <para>This is one approach to optimize deserializing large json strings.</para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async static Task<T?> FetchAndJsonDeserializeAsync<T>(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (Stream responseString = await client.GetStreamAsync($"{url}"))
+                {
+                    using (StreamReader streamReader = new StreamReader(responseString))
+                    {
+                        using (JsonReader reader = new JsonTextReader(streamReader))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+
+                            // read the json from a stream
+                            // json size doesn't matter because only a small piece is read at a time from the HTTP request
+                            return await Task.Run(() => serializer.Deserialize<T>(reader));
+                        }
+                    }
+                }
             }
         }
     }
