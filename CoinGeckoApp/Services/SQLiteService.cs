59f56088a4a08b673edaa916e04d1346c5ff7a68
ca20110820 <ca20110820@gmail.com>
2024-03-28 14:16:21 +0800

    installed Microsoft.Data.Sqlite

---
+++ CoinGeckoApp/Services/SQLiteService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGeckoApp.Services
+{
+    public class SQLiteService
+    {
+
+        /* Constructors */
+        public SQLiteService()
+        {
+
+        }
+
+
+        public async Task ExecuteNonQueryAsync(string query)
+        {
+
+        }
+    }
+}
