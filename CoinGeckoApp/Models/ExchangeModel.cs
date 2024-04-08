dee8003e477e3a61a5bfb8b3bc2fbb397ebad02d
ca20110820 <ca20110820@gmail.com>
2024-04-08 13:33:00 +0800

    feat(ExchangeModel): added ExchangeModel class

---
+++ CoinGeckoApp/Models/ExchangeModel.cs
@@ -0,0 +1,42 @@
+﻿using CoinGeckoApp.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGeckoApp.Models
+{
+    public class ExchangeModel
+    {
+        
+        public string Id { get; set; }
+
+
+        public ExchangeModel() { }
+        public ExchangeModel(string id) 
+        {  
+            Id = id; 
+        }
+
+
+        public static async Task<List<Dictionary<string, string>>?> FetchExchangeIds()
+        {
+            /* Reponse Form:
+             * [{"id": "<value>", "name": "<value>"}, 
+             * {"id": "<value>", "name": "<value>"}, 
+             * ...]
+             */
+            string url = "https://api.coingecko.com/api/v3/exchanges/list";
+            return await APIHelper.FetchAndJsonDeserializeAsync<List<Dictionary<string, string>>>(url);
+        }
+        public static async Task<List<string>?> GetExchangeIds()
+        {
+            List<Dictionary<string, string>>? exchanges = await FetchExchangeIds();
+            if (exchanges == null) return null;
+            return exchanges.Select(exchange => exchange["id"]).ToList();
+        }
+    }
+}
