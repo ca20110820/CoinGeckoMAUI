3423a39161db9a6c4511277bcaf0ce9d78cd574b
ca20110820 <ca20110820@gmail.com>
2024-04-08 09:39:43 +0800

    feat(IUpdatableView): added SetModelAsync abstract method

--- CoinGeckoApp/Views/IUpdatableView.cs
+++ CoinGeckoApp/Views/IUpdatableView.cs
@@ -8,6 +8,16 @@ namespace CoinGeckoApp.Views
 {
     public interface IUpdatableView
     {
-        Task UpdateUI();
+        /// <summary>
+        /// Manual update of UI based on changes in a Model.
+        /// <para>This method can be used as an alternative to Data Binding for rendering the UI.</para>
+        /// </summary>
+        Task UpdateUIAsync();
+
+        /// <summary>
+        /// Manually sets a new model in context inside a View (i.e. Page) or outside the View (e.g. Controller or ViewModel classes).
+        /// </summary>
+        /// <param name="model"></param>
+        Task SetModelAsync(object model);
     }
 }
