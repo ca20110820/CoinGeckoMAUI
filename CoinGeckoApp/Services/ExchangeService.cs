04335ed39d12ea7cbddf98f3afd08f74d252771d
ca20110820 <ca20110820@gmail.com>
2024-04-08 13:40:09 +0800

    feat(ExchangeService): added initial implementation of ExchangeService class

---
+++ CoinGeckoApp/Services/ExchangeService.cs
@@ -0,0 +1,23 @@
+﻿using CoinGeckoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGeckoApp.Services
+{
+    public class ExchangeService
+    {
+
+        public ExchangeModel Exchange {  get; set; }
+
+        public ExchangeService() { }
+        public ExchangeService(ExchangeModel exchange)
+        {
+            Exchange = exchange;
+        }
+
+
+    }
+}
