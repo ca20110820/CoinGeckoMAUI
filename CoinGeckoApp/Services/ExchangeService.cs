15405008b3b9864ec02904835a8c12b7cf83544b
ca20110820 <ca20110820@gmail.com>
2024-04-08 14:42:32 +0800

    feat(ExchangeService): added methods for Fetching APIExchangesIdResponse and APIExchangeIdTickersResponse

--- CoinGeckoApp/Services/ExchangeService.cs
+++ CoinGeckoApp/Services/ExchangeService.cs
@@ -1,4 +1,6 @@
 ﻿using CoinGeckoApp.Models;
+using CoinGeckoApp.Helpers;
+using CoinGeckoApp.Responses.Exchanges;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,8 @@ namespace CoinGeckoApp.Services
 {
     public class ExchangeService
     {
+        private URIHelper uriHelper = new("https://api.coingecko.com");
+        private string _endpoint = URIHelper.MakeEndpoint("api", "v3", "exchanges");
 
         public ExchangeModel Exchange {  get; set; }
 
@@ -19,5 +23,17 @@ namespace CoinGeckoApp.Services
         }
 
 
+        public async Task<APIExchangesIdResponse?> FetchExchangeIdResponseAsync()
+        {
+            // Example: https://api.coingecko.com/api/v3/exchanges/binance
+            string uri = $"https://api.coingecko.com/api/v3/exchanges/{Exchange.Id}";
+            return await APIHelper.FetchAndJsonDeserializeAsync<APIExchangesIdResponse>(uri);
+        }
+        public async Task<APIExchangeIdTickersResponse?> FetchExchangeTickers(int page = 1)
+        {
+            // Example: https://api.coingecko.com/api/v3/exchanges/<id>/tickers?include_exchange_logo=true&page=<page-num>&depth=true&order=trust_score_desc
+            string uri = $"https://api.coingecko.com/api/v3/exchanges/{Exchange.Id}/tickers?include_exchange_logo=true&page={page}&depth=true&order=trust_score_desc";
+            return await APIHelper.FetchAndJsonDeserializeAsync<APIExchangeIdTickersResponse>(uri);
+        }
     }
 }
